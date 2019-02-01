aa3210be5ca16b489e46bebf0194323aa75da82b
srikanthpragada <srikanthpragada@gmail.com>
2019-02-01 17:59:30 +0530

    01-FEB

--- oop/product.py
+++ oop/product.py
@@ -1,18 +1,27 @@
 class Product:
+    # Class attributes - static variable
+    tax = 18
+    @staticmethod
+    def set_tax(newtax):
+        Product.tax = newtax
+
     # Constructor
     def __init__(self, name, price, qoh=0):
         # Object attributes
         self.name = name
-        self.__price = price
+        self.price = price # Name Wrangling
         self.qoh = qoh
 
     # Methods
     def print_details(self):
-        print(f"Name = {self.name}, Price = {self.__price}, Qoh = {self.qoh}")
+        print(f"Name = {self.name}, Price = {self.price}, Qoh = {self.qoh}")
+
+    def net_price(self):
+        return self.price + (self.price * Product.tax / 100)
 
     # Special methods
     def __str__(self):
-        return f"{self.name},{self.__price},{self.qoh}"
+        return f"{self.name},{self.price},{self.qoh}"
 
     def __eq__(self,other):
         print('__eq__')
@@ -24,14 +33,12 @@ class Product:
 
 # Create object
 p1 = Product("iPhone X", 80000)
-p2 = Product("iPhone X", 70000)
+print(p1.net_price())
+Product.set_tax(20)
+print(p1.net_price())
 
-print( getattr(p1,'qoh',10))
 
-print(p1)  # str(p1)
-print(p1 == p2)
-print(p1 != p2)
-print(p1 > p2)
+
 
 
 
